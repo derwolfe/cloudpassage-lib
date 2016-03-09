255928574a8b63bf64975c6dae44fc01890a32ed
Elana Hashman <elana.hashman@rackspace.com>
2016-03-09 19:56:06 +0000

    Actually include the plugins Travis needs

--- project.clj
+++ project.clj
@@ -3,4 +3,14 @@
   :url "https://github.com/RackSec/cloudpassage-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]])
+  :dependencies [[org.clojure/clojure "1.8.0"]]
+  :plugins [[lein-cljfmt "0.3.0"]
+            [lein-pprint "1.1.1"]
+            [lein-cloverage "1.0.7-SNAPSHOT"]]
+  :min-lein-version "2.0.0"
+  :main ^:skip-aot cloudpassage-lib.core
+  :uberjar-name "cloudpassage-lib.jar"
+  :profiles {:uberjar {:aot :all}
+             :dev {:dependencies [[pjstadig/humane-test-output "0.7.1"]]
+                   :injections [(require 'pjstadig.humane-test-output)
+                                (pjstadig.humane-test-output/activate!)]}})
