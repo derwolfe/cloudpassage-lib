d21c969d4b31723d948a8cbab3cd01e127b11739
Chris Wolfe <chriswwolfe@gmail.com>
2016-03-09 16:00:46 -0600

    add redis and fernet

--- src/cloudpassage_lib/core.clj
+++ src/cloudpassage_lib/core.clj
@@ -2,18 +2,30 @@
   (:require
    [clojure.string :as str]
    [aleph.http :as http]
+   [environ.core :refer [env]]
    [manifold.deferred :as md]
    [manifold.time :as mt]
    [byte-streams :as bs]
+   [taoensso.carmine :as car :refer (wcar)]
    [taoensso.timbre :as timbre :refer [info error]]
    [base64-clj.core :as base64]
    [clj-time.core :as time]
    [clj-time.format :as f]
+   [cloudpassage-lib.fernet :as fernet]
    [cheshire.core :as json]))
 
 ;; the url from which new auth-tokens can be obtained.
 (def auth-uri "https://api.cloudpassage.com/oauth/access_token?grant_type=client_credentials")
 (def events-uri "https://api.cloudpassage.com/v1/events?")
+
+(defn redis-connection
+  []
+  (let [{:keys [redis-url redis-timeout]} env]
+    {:pool {}
+     :spec {:uri redis-url
+            :timeout (read-string redis-timeout)}}))
+
+(defmacro wcar* [& body] `(car/wcar (redis-connection) ~@body))
 
 (defn ^:private ->basic-auth-header
   [client-id client-key]
@@ -71,3 +83,25 @@
       (fn [exc]
         (error "error fetching events page:" (.getMessage exc))
         ::fetch-error)))))
+
+(defn fetch-token!
+  "Fetch an access token for the cloudpassage api that belongs to the client-id/secret pair.
+   If a token doesn't exist in Redis, then this will hit the cloudpassage api to obtain one.
+   If the token exists in the cache, it will be returned.
+
+  Returns a string representing an access-token."
+  [client-id client-secret fernet-key]
+  (let [account-key (str "account-" client-id)
+        token (wcar* (car/get account-key))]
+    (if (some? token)
+      ;; a token is in redis
+      (fernet/decrypt fernet-key token)
+      ;; no token is present, fetch a new one
+      (let [new-token (get-auth-token! client-id client-secret)
+            ;; this will cause the token to expire 100 seconds earlier than expiration
+            ;; it is a simple fudge factor.
+            {:keys [access_token expires_in]} new-token
+            ttl (- expires_in 100)
+            encrypted-token (fernet/encrypt fernet-key access_token)]
+        (wcar* (car/setex account-key ttl encrypted-token))
+        access_token))))
---
+++ test/cloudpassage_lib/redis_test.clj
@@ -0,0 +1,95 @@
+(ns cloudpassage-lib.redis-test
+  (:require [clojure.test :refer :all]
+            [clj-time.core :as ct]
+            [clj-time.coerce :as c]
+            [taoensso.carmine :as car]
+            [fernet.core :as fernet-clj]
+            [cloudpassage-lib.fernet :as cp-fernet]
+            [cloudpassage-lib.core :as core]))
+
+(defn ^:private flush-redis! []
+  (core/wcar* (car/flushall)))
+
+(deftest fetch-token!-tests
+  (let [fernet-key (fernet-clj/generate-key)]
+    (testing "encrypts new api token with fernet"
+      (let [client-id "client-id"
+            token-key (str "account-" client-id)
+            called (atom false)
+            fake-token {:expires_in 900
+                        :token_type "bearer"
+                        :access_token "12"}
+            expected-token (:access_token fake-token)
+            get-fake-token (fn [_ _] fake-token)]
+        (with-redefs [core/get-auth-token! get-fake-token]
+          (flush-redis!)
+          ;; this sets and returns the token.
+          ;; a side effect is the token being stored in redis
+          (core/fetch-token! client-id "unused secret" fernet-key)
+          (is (not= fake-token (core/wcar* (car/get token-key)))
+              "plaintext not stored in redis")
+          (is (= "12" (core/fetch-token! client-id "unused secret" fernet-key))
+              "returns the plaintext access token"))))
+    (testing "returns cached token when present"
+      (let [client-id "client-id"
+            token-key (str "account-" client-id)
+            called (atom false)
+            fake-token {:expires_in 900
+                        :token_type "bearer"
+                        :access_token "12"}
+            expected-token (:access_token fake-token)
+            encrypted-token (cp-fernet/encrypt fernet-key expected-token)
+            get-fake-token (fn [_ _]
+                             (reset! called false)
+                             "if called, I should explode")]
+        (with-redefs [core/get-auth-token! get-fake-token]
+          (flush-redis!)
+          (core/wcar* (car/set token-key encrypted-token))
+          (is (= expected-token (core/fetch-token! client-id "unused-secret" fernet-key)))
+          (is (false? @called)))))
+    (testing "expires token after expiration"
+      (let [client-id "client-id"
+            token-key (str "account-" client-id)
+            fake-token {:expires_in 101
+                        :token_type "bearer"
+                        :access_token "12"}
+            get-fake-token (fn [_ _] fake-token)]
+        (with-redefs [core/get-auth-token! get-fake-token]
+          (flush-redis!)
+          ;; cause the caching side-effect
+          (core/fetch-token! client-id "unused-secret" fernet-key)
+          ;; the token should have been purged from redis as its ttl was 1
+          ;; wait a real second for redis to purge the key. This is gross
+          ;; but is the only real way to test the interaction with redis.
+          (Thread/sleep 1000)
+          (is (nil? (core/wcar* (car/get token-key)))))))
+    (testing "sets expiration of token to 100 seconds less than actual"
+      (let [client-id "client-id"
+            token-key (str "account-" client-id)
+            fake-token {:expires_in 900
+                        :token_type "bearer"
+                        :access_token "12"}
+
+            get-fake-token (fn [_ _] fake-token)]
+        (with-redefs [core/get-auth-token! get-fake-token]
+          (flush-redis!)
+          ;; cause the caching side-effect
+          (core/fetch-token! client-id "unused-secret" fernet-key)
+          ;; this might not be a good assertion; as the test could
+          ;; hang, and cause the assertion to fail.
+          (is (= 800 (core/wcar* (car/ttl token-key)))))))
+    (testing "fetches new token when needed"
+      (let [client-id "clientid"
+            token-key (str "account" client-id)
+            fake-token {:expires_in 900
+                        :token_type "bearer"
+                        :access_token "12"}
+            expected-token (:access_token fake-token)
+            get-fake-token (fn [_ _] fake-token)]
+        (with-redefs [core/get-auth-token! get-fake-token]
+          (flush-redis!)
+          ;; there is no token in redis
+          (is (nil? (core/wcar* (car/get token-key)))
+              "the key is not in redis")
+          (is (= expected-token (core/fetch-token! client-id "unused-secret" fernet-key))
+              "returns the token provided by the fake http call"))))))
