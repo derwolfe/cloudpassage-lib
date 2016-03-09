ceeaea71f8bb51939c1a81c4e5bfe662e081d097
Chris Wolfe <chriswwolfe@gmail.com>
2016-03-09 14:36:01 -0600

    Start extracting functionality
    This is the first pass at pulling out logic.
    
    Includes
    - get-auth-token! - this is meant to fetch a token from their api
      using the secret key and id provided by cp. How this is persisted
      is up to the consumer.
    - get-single-events-page! - used to issue requests against the cp
      api.
    - ->cp-date - function for formatting dates in way that their api
      expects.

--- project.clj
+++ project.clj
@@ -1,16 +1,21 @@
 (defproject cloudpassage-lib "0.1.0-SNAPSHOT"
-  :description "Clojure interface for the CloudPassage API"
-  :url "https://github.com/RackSec/cloudpassage-lib"
+  :description "A library for interacting with cloudpassage apis."
+  :url "http://github.com/RackSec/cloudpassage-lib"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.8.0"]]
-  :plugins [[lein-cljfmt "0.3.0"]
+  :dependencies [[org.clojure/clojure "1.8.0"]
+                 [com.taoensso/timbre "4.2.0"]
+                 [manifold "0.1.2"]
+                 [aleph "0.4.1-beta2"]
+                 [clj-time "0.11.0"]
+                 [base64-clj "0.1.1"]
+                 [cheshire "5.5.0"]
+                 [com.cemerick/url "0.1.1"]]
+  :plugins [[lein-auto "0.1.2"]
+            [lein-cljfmt "0.3.0"]
             [lein-pprint "1.1.1"]
             [lein-cloverage "1.0.7-SNAPSHOT"]]
   :min-lein-version "2.0.0"
-  :main ^:skip-aot cloudpassage-lib.core
-  :uberjar-name "cloudpassage-lib.jar"
-  :profiles {:uberjar {:aot :all}
-             :dev {:dependencies [[pjstadig/humane-test-output "0.7.1"]]
+  :profiles {:dev {:dependencies [[pjstadig/humane-test-output "0.7.1"]]
                    :injections [(require 'pjstadig.humane-test-output)
                                 (pjstadig.humane-test-output/activate!)]}})
--- test/cloudpassage_lib/core_test.clj
+++ test/cloudpassage_lib/core_test.clj
@@ -1,3 +1,47 @@
 (ns cloudpassage-lib.core-test
   (:require [clojure.test :refer :all]
-            [cloudpassage-lib.core :refer :all]))
+            [manifold.deferred :as md]
+            [clj-time.core :as ct]
+            [cheshire.core :as json]
+            [cloudpassage-lib.core :as core]))
+
+(deftest get-auth-token!-tests
+  (testing "returns an authentication token"
+    (let [sent-at (ct/now)
+          expires-at (ct/plus sent-at (ct/seconds 900))
+          response {:access_token "ffad76cc550110fc4c84a18397b6e104"
+                    :expires_in 900
+                    :token_type "bearer"}
+          fake-post (fn [_addr _opts]
+                      (let [foo (md/deferred)]
+                        (md/success!
+                         foo
+                         {:body (json/generate-string response)})
+                        foo))]
+      (with-redefs [aleph.http/post fake-post
+                    clj-time.core/now (fn [] sent-at)]
+        (is (= response (core/get-auth-token! "secret-key" "id")))))))
+
+(deftest iso-date-tests
+  (testing "it actually formats dates"
+    (let [a-date (ct/date-time 1986 10 14 4 3 27 456)
+          formatted (core/->cp-date a-date)]
+      (is (= formatted "1986-10-14T04:03:27Z")))))
+
+(deftest get-single-events-page!-tests
+  (testing "returns ::fetch-error on exception"
+    (let [fake-get (fn [_addr _headers]
+                     (let [d (md/deferred)]
+                       (md/error! d (Exception. "kaboom"))
+                       d))]
+      (with-redefs [aleph.http/get fake-get]
+        (is (= :cloudpassage-lib.core/fetch-error
+               @(core/get-single-events-page! "" ""))))))
+  (testing "returns deserialized json body"
+    (let [fake-get (fn [_addr _headers]
+                     (let [d (md/deferred)]
+                       (md/success! d {:body (json/generate-string {:events []})})
+                       d))]
+      (with-redefs [aleph.http/get fake-get]
+        (is (= {:events []}
+               @(core/get-single-events-page! "" "")))))))
