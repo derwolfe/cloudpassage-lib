b3a109bfad313b2a7fae82eea20f0bdb929ea7fb
Chris Wolfe <chriswwolfe@gmail.com>
2016-03-10 14:43:33 -0600

    lein-release plugin: preparing 0.1.0 release

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cloudpassage-lib "0.1.0-SNAPSHOT"
+(defproject cloudpassage-lib "0.1.0"
   :description "A library for interacting with cloudpassage apis."
   :url "http://github.com/RackSec/cloudpassage-lib"
   :license {:name "Eclipse Public License"
