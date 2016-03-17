db25e4e6631e5d9b73a599a4dc17b939ea00ba9d
Chris Wolfe <chriswwolfe@gmail.com>
2016-03-17 10:45:44 -0500

    Version 0.1.7

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cloudpassage-lib "0.1.7-SNAPSHOT"
+(defproject cloudpassage-lib "0.1.7"
   :description "A library for interacting with cloudpassage apis."
   :lein-release {:deploy-via :clojars}
   :url "http://github.com/RackSec/cloudpassage-lib"
