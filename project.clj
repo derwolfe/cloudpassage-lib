f4c93e9ced7736d0245c4c3242b52bc0f3be7342
Elana Hashman <elana.hashman@rackspace.com>
2016-04-04 10:01:48 -0400

    lein-release plugin: bumped version from 0.2.2 to 0.2.3-SNAPSHOT for next development cycle

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject cloudpassage-lib "0.2.2"
+(defproject cloudpassage-lib "0.2.3-SNAPSHOT"
   :description "A library for interacting with cloudpassage apis."
   :lein-release {:deploy-via :clojars}
   :url "http://github.com/RackSec/cloudpassage-lib"
